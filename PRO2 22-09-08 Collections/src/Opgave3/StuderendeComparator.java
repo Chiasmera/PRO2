e9347c4da7c43b02294a7215c1a6f7f97925bb8d
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2022-09-15 13:19:06 +0200

    22-09-15

--- PRO2 22-09-08 Collections/src/Opgave3/StuderendeComparator.java
+++ PRO2 22-09-08 Collections/src/Opgave3/StuderendeComparator.java
@@ -5,12 +5,17 @@ import java.util.Comparator;
 public class StuderendeComparator implements Comparator<Studerende> {
     @Override
     public int compare(Studerende s1, Studerende s2) {
+
+
         int value = 0;
-        if (s1.getNavn().compareTo(s2.getNavn()) < 0) {
+        value = s1.getNavn().compareTo(s2.getNavn());
+
+ /*       if (s1.getNavn().compareTo(s2.getNavn()) < 0) {
             value = -1;
         } else if (s1.getNavn().compareTo(s2.getNavn()) > 0) {
             value = 1;
-        }
+        }*/
+
         return value;
     }
 }
